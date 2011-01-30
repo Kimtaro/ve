d45b1fec02f08399a82b9723fbd77e108c9a69a3
Kim Ahlström <kim.ahlstrom@gmail.com>
2011-01-30 16:25:12 +0900

    Switch the JS to POST because GET sucks ass

--- sinatra/server.rb
+++ sinatra/server.rb
@@ -6,7 +6,12 @@ require File.expand_path(File.dirname(__FILE__) + "/../sprakd/lib/sprakd")
 
 get '/:language/words' do
   words = Sprakd.get(params[:text], params[:language], :words)
+
+  result = JSON.generate(words.collect(&:as_json))
+  if params[:callback]
+    result = "#{params[:callback]}(#{result})"
+  end
   
-  "#{params[:callback]}(#{JSON.generate(words.collect(&:as_json))})"
+  result
 end
 
