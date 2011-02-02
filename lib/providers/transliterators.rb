e0c33d02f1e51e8a2932ab62262932955d322d19
Kim Ahlström <kim.ahlstrom@gmail.com>
2011-02-03 00:39:52 +0900

    Starting to work on hira to latn transliteration

--- lib/providers/transliterators.rb
+++ lib/providers/transliterators.rb
@@ -25,6 +25,53 @@ class Sprakd
   class Parse
     class Transliterators < Sprakd::Parse
 
+      HIRA_TO_LATN = {
+        "あ"=>"a", "い"=>"i", "う"=>"u", "え"=>"e", "お"=>"o",
+        "か"=>"ka", "き"=>"ki", "く"=>"ku", "け"=>"ke", "こ"=>"ko",
+        "が"=>"ga", "ぎ"=>"gi", "ぐ"=>"gu", "げ"=>"ge", "ご"=>"go",
+        "さ"=>"sa", "し"=>"shi", "す"=>"su", "せ"=>"se", "そ"=>"so",
+        "ざ"=>"za", "じ"=>"ji", "ず"=>"zu", "ぜ"=>"ze", "ぞ"=>"zo",
+        "た"=>"ta", "ち"=>"chi", "つ"=>"tsu", "て"=>"te", "と"=>"to",
+        "だ"=>"da", "ぢ"=>"ji", "づ"=>"zu", "で"=>"de", "ど"=>"do",
+        "な"=>"na", "に"=>"ni", "ぬ"=>"nu", "ね"=>"ne", "の"=>"no",
+        "は"=>"ha", "ひ"=>"hi", "ふ"=>"fu", "へ"=>"he", "ほ"=>"ho",
+        "ば"=>"ba", "び"=>"bi", "ぶ"=>"bu", "べ"=>"be", "ぼ"=>"bo",
+        "ぱ"=>"pa", "ぴ"=>"pi", "ぷ"=>"pu", "ぺ"=>"pe", "ぽ"=>"po",
+        "ま"=>"ma", "み"=>"mi", "む"=>"mu", "め"=>"me", "も"=>"mo",
+        "や"=>"ya", "ゆ"=>"yu", "よ"=>"yo",
+        "ら"=>"ra", "り"=>"ri", "る"=>"ru", "れ"=>"re", "ろ"=>"ro",
+        "わ"=>"wa", "うぃ"=>"whi", "うぇ"=>"whe", "を"=>"wo",
+        "ゑ"=>"wye", "ゐ"=>"wyi", "ー"=>"-", "ん"=>"n",
+
+        "きゃ"=>"kya", "きゅ"=>"kyu", "きょ"=>"kyo", "きぇ"=>"kye", "きぃ"=>"kyi",
+        "ぎゃ"=>"gya", "ぎゅ"=>"gyu", "ぎょ"=>"gyo", "ぎぇ"=>"gye", "ぎぃ"=>"gyi",
+        "くぁ"=>"kwa", "くぃ"=>"kwi", "くぅ"=>"kwu", "くぇ"=>"kwe", "くぉ"=>"kwo",
+        "ぐぁ"=>"qwa", "ぐぃ"=>"gwi", "ぐぅ"=>"gwu", "ぐぇ"=>"gwe", "ぐぉ"=>"gwo",
+        "しゃ"=>"sha", "しぃ"=>"syi", "しゅ"=>"shu", "しぇ"=>"she", "しょ"=>"sho",
+        "じゃ"=>"jya", "じゅ"=>"zyu", "じぇ"=>"zye", "じょ"=>"zyo", "じぃ"=>"zyi",
+        "すぁ"=>"swa", "すぃ"=>"swi", "すぅ"=>"swu", "すぇ"=>"swe", "すぉ"=>"swo",
+        "ちゃ"=>"tya", "ちゅ"=>"tyu", "ちぇ"=>"tye", "ちょ"=>"tyo", "ちぃ"=>"tyi",
+        "ぢゃ"=>"dya", "ぢぃ"=>"dyi", "ぢゅ"=>"dyu", "ぢぇ"=>"dye", "ぢょ"=>"dyo",
+        "つぁ"=>"tsa", "つぃ"=>"tsi", "つぇ"=>"tse", "つぉ"=>"tso", "てゃ"=>"tha",
+        "てぃ"=>"thi", "てゅ"=>"thu", "てぇ"=>"the", "てょ"=>"tho", "とぁ"=>"twa",
+        "とぃ"=>"twi", "とぅ"=>"twu", "とぇ"=>"twe", "とぉ"=>"two", "でゃ"=>"dha",
+        "でぃ"=>"dhi", "でゅ"=>"dhu", "でぇ"=>"dhe", "でょ"=>"dho", "どぁ"=>"dwa",
+        "どぃ"=>"dwi", "どぅ"=>"dwu", "どぇ"=>"dwe", "どぉ"=>"dwo", "にゃ"=>"nya",
+        "にゅ"=>"nyu", "にょ"=>"nyo", "にぇ"=>"nye", "にぃ"=>"nyi", "ひゃ"=>"hya",
+        "ひぃ"=>"hyi", "ひゅ"=>"hyu", "ひぇ"=>"hye", "ひょ"=>"hyo", "びゃ"=>"bya",
+        "びぃ"=>"byi", "びゅ"=>"byu", "びぇ"=>"bye", "びょ"=>"byo", "ぴゃ"=>"pya",
+        "ぴぃ"=>"pyi", "ぴゅ"=>"pyu", "ぴぇ"=>"pye", "ぴょ"=>"pyo", "ふぁ"=>"fwa",
+        "ふぃ"=>"fyi", "ふぇ"=>"fye", "ふぉ"=>"fwo", "ふぅ"=>"fwu", "ふゃ"=>"fya",
+        "ふゅ"=>"fyu", "ふょ"=>"fyo", "みゃ"=>"mya", "みぃ"=>"myi", "みゅ"=>"myu",
+        "みぇ"=>"mye", "みょ"=>"myo", "りゃ"=>"rya", "りぃ"=>"ryi", "りゅ"=>"ryu",
+        "りぇ"=>"rye", "りょ"=>"ryo",
+        "ゔぁ"=>"va", "ゔぃ"=>"vyi", "ゔ"=>"vu", "ゔぇ"=>"vye", "ゔぉ"=>"vo",
+        "ゔゃ"=>"vya", "ゔゅ"=>"vyu", "ゔょ"=>"vyo",
+        "うぁ"=>"wha", "いぇ"=>"ye", "うぉ"=>"who",
+        "ぁ"=>"xa", "ぃ"=>"xi", "ぅ"=>"xu", "ぇ"=>"xe", "ぉ"=>"xo",
+        "ゕ"=>"xka", "ゖ"=>"xke", "ゎ"=>"xwa"
+       }
+
       attr_reader :tokens, :text
 
       def initialize(text)
@@ -32,8 +79,37 @@ class Sprakd
         @text = text
       end
 
-      def transliterate_from_kana_to_latn
-        return @text
+      def transliterate_from_hira_to_latn
+        # Hepburn style romaji
+        kana = @text.dup
+        romaji = ''
+
+        while kana.length > 0
+          [3, 2, 1].each do |length|
+            mora = ''
+            for_removal = length
+            for_conversion = kana[0, length]
+            
+            if for_conversion == 'っ'
+              # TODO
+            elsif HIRA_TO_LATN[for_conversion]
+              # Generic cases
+              mora = HIRA_TO_LATN[for_conversion]
+            end
+
+            if mora.length > 0
+              romaji << mora
+              kana[0, for_removal] = ''
+              break
+            elsif length == 1
+              # Nothing found
+              romaji << for_conversion
+              romaji[0, for_removal] = ''
+            end
+          end
+        end
+
+        return romaji
       end
       
       def transliterate_from_hira_to_kata
--- tests/transliterators_test.rb
+++ tests/transliterators_test.rb
@@ -12,6 +12,16 @@ class TransliteratorsTest < Test::Unit::TestCase
     trans = Sprakd::Provider::Transliterators.new
     assert trans.works?
   end
+
+  def test_transliterate_from_hira_to_latn
+    trans = Sprakd::Provider::Transliterators.new
+    assert_equal 'kosoado',   trans.parse('こそあど').transliterate_from_hira_to_latn
+    assert_equal 'konna',     trans.parse('こんな').transliterate_from_hira_to_latn
+    assert_equal 'konyaku',   trans.parse('こにゃく').transliterate_from_hira_to_latn
+    assert_equal 'kon\'yaku', trans.parse('こんやく').transliterate_from_hira_to_latn
+    assert_equal 'shinbun',   trans.parse('しんぶん').transliterate_from_hira_to_latn
+    assert_equal 'appa',      trans.parse('あっぱ').transliterate_from_hira_to_latn
+  end
   
   def test_transliterate_from_hira_to_kata
     trans = Sprakd::Provider::Transliterators.new
