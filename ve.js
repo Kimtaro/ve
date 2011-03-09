3c8fb0fe60ff2646738a867c7825c5dcf03708f5
Kim Ahlström <kim.ahlstrom@gmail.com>
2011-03-09 01:34:16 -0800

    Working demo on the main page

--- ve.js
+++ ve.js
@@ -1,12 +1,14 @@
 Ve = function(language) {
   return({
     'words': function(text, callback) {
-      //var url = 'http://ve.kimtaro.com:4567/' + language + '/words';
-      var url = 'http://localhost:4567/' + language + '/words';
+      var url = 'http://ve.kimtaro.com:4567/' + language + '/words';
+      // var url = 'http://localhost:4567/' + language + '/words';
+      Ve.cb = callback;
+      
       $.ajax({
         url: url,
-        data: {'text': text},
-        dataType: 'json',
+        data: {'text': text, 'callback': 'Ve.cb'},
+        dataType: 'jsonp',
 		    type: 'POST',
         success: function(data){
           callback(data);
